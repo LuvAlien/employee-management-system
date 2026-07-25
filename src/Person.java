646ff3906f4344bb5bf4dd858cac1c830695178d
cmyers-sh <cmyers.shahmat@gmail.com>
2026-07-25 16:59:58 -0400

    Update Person.java
    Altered to fit class diagram spec.

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,16 @@
 public class Person {
+    String firstName;
+    String lastName;
+    int age;
+    
+    public Person(String fn, String ln, int a) {
+        this.firstName = fn;
+        this.lastName = ln;
+        this.age = a;
+    }
+    
+    public String getFullName() {
+        return firstName + " " + lastName;
+    }
     
 }
